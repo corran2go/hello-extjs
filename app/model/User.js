62c6c184b7a8bd4c2472f645624b4720a988cd7e
Steffen Werner <corran2go@gmail.com>
2011-09-23 09:52:07 +0200

    moved proxy from store to model

--- app/model/User.js
+++ app/model/User.js
@@ -1,4 +1,18 @@
 Ext.define('AM.model.User', {
   extend: 'Ext.data.Model',
-  fields: [ 'name', 'email' ]
+
+  fields: [ 'name', 'email' ],
+  
+  proxy: {
+    type: 'ajax',
+    api: {
+      read: 'data/users.json',
+      update: 'data/updateUsers.json'
+    },
+    reader: {
+      type: 'json',
+      root: 'users',
+      successProperty: 'success'
+    }
+  }
 });
